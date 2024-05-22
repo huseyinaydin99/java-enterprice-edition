c25e223d037c0d5d28940063614138c0f3d88ba6
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-22 12:42:45 +0300

    Java EE - Java Web Teknolojilerinin en temeli olan Servlet ile Java kodunda HTML, CSS ve JavaScript yazılması.

---
+++ 01_Servlets/src/tr/com/huseyinaydin/ServletDeneme.java
@@ -0,0 +1,38 @@
+package tr.com.huseyinaydin;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@WebServlet("/anasayfa")
+public class ServletDeneme extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public ServletDeneme() {
+		super();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append(" Servlet bilgisi: ").append(request.getContextPath());
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		super.doPost(request, response);
+	}
+}
---
+++ 01_Servlets/src/tr/com/huseyinaydin/ServletSinifi.java
@@ -0,0 +1,61 @@
+package tr.com.huseyinaydin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Spring Boot Security
+*
+*/
+
+@WebServlet(name = "ServletSinifi", urlPatterns = { "/ServletSinifi" })
+public class ServletSinifi extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	public ServletSinifi() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		PrintWriter out = response.getWriter();
+		try {
+			String kullanciAdi = request.getParameter("kullanciAdi").toString();
+			String parola = request.getParameter("parola").toString();
+
+			out.println("<html>");
+			out.println("<head>");
+			out.println("<meta http-equiv=Content-Type content=text/html; charset=UTF-8>");
+			out.println("<title>Merkez</title>");
+			out.println("</head>");
+			out.println("<body>");
+
+			out.println("<h1>Servlet : " + request.getContextPath() + "</h1>");
+			out.println("<h2> Kullanıcı Adı : <font color=red>" + kullanciAdi + "</font> HOŞ GELDİNİZ</h2>");
+			out.println("<h2> Parola        : <font color=red>" + parola + "</font> </h2>");
+
+			out.println("</body>");
+			out.println("</html>");
+		} finally {
+			out.close();
+		}
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
