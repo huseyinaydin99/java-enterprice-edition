2e238c4240c367c829a6c58618f8f2f500b02e2b
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-30 10:35:39 +0300

    Java EE - SOAP Web Servisleri istemci.

---
+++ 13_SOAP-Istemci-Konsol/src/tr/com/huseyinaydin/Istemci.java
@@ -0,0 +1,25 @@
+package tr.com.huseyinaydin;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java EE
+*
+*/
+
+public class Istemci {
+	
+	public static void main(String[] args) throws Exception  {
+		SelamWSStub stub = new SelamWSStub();
+		
+		//Web servisine istegimizi yapalim.
+		SelamWSStub.SelamVer istek = new SelamWSStub.SelamVer();
+		istek.setAdiSoyadi("Hüseyin AYDIN");
+		
+		//WebServisini cagiralim ve cavbimizi alalim.
+		SelamWSStub.SelamVerResponse cevap = stub.selamVer(istek);
+		System.out.println("WEB SERVISINDEN GELEN CEVAP: " + cevap.get_return());
+	}
+}
