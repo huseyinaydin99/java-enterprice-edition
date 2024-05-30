991ea55cd83a1a9330a03c4d39f2f8b9962d4b2c
huseyinaydin99 <huseyinaydin99@gmail.com>
2024-05-30 10:33:52 +0300

    Java EE - SOAP Web Servisleri sunucu.

---
+++ 12_SOAP-Sunucu/src/tr/com/huseyinaydin/SelamWS.java
@@ -0,0 +1,17 @@
+package tr.com.huseyinaydin;
+
+//بسم الله الرحمن الرحيم
+/**
+*
+* @author Huseyin_Aydin
+* @since 1994
+* @category Java EE
+*
+*/
+
+public class SelamWS {
+
+	public String selamVer(String adiSoyadi) {
+		return "Selamlar " + adiSoyadi + " kardeş nöğrün?";
+	}
+}
